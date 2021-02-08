f132eb5ab9fe28a19aa3e90a3ad8d71bf8e427d9
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-08 15:59:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace promedio_matriz
+
+    
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+             {
+            double sumatoria = 0.0;
+            double promedio = 0.0;
+            Random rnd = new Random();
+            int n = 0, m = 0; 
+
+            int[,] datos = new int[4,7];
+
+            //llenamos los datos
+
+            for (n=0; n<4; n++)
+            {
+                for(m=0; m < 7; m++)
+                {
+                    datos[n, m] = rnd.Next(20);
+                }
+            }
+            
+          //imprimir datos
+
+            for(n=0; n<4;  n++)
+            {
+                Console.Write("semana {0} ",  n);
+                
+                  
+
+                for(m=0;m<7;m++)
+                {
+                    Console.Write(" {0} ",  n);
+
+                }
+                Console.WriteLine();
+
+            }
+            
+            //calculamos el promedio por semana
+
+            for(n=0;n<4;n++)
+            {
+                sumatoria = 0.0;
+               promedio = 0.0;
+                    
+
+                for(m=0;m<7; m++)
+                {
+                    sumatoria += datos[n, m];
+                    
+                    
+                }
+
+                promedio = sumatoria / 7.0;
+                Console.Write("el promedio {0} es  {1} ", n, promedio);
+
+
+                // calculamos promedio total x mes
+
+                
+
+                   
+            }
+            sumatoria = 0.0;
+            promedio = 0.0;
+
+            for (n = 0; n < 4; n++)
+            {
+                for (m = 0; m < 7; m++)
+                {
+                    sumatoria += datos[n , m];
+                }
+
+
+            }
+
+            promedio = sumatoria / 28.0;
+
+            Console.Write(" el promedio total  {0} ", promedio);
+        }
+    }
+}
